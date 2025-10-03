57c8ee14a0767481b26155606bf33f3fe3179dbf
GABRIELLE BUENO VIEIRA <gabrielle.bvieira1@senacsp.edu.br>
2025-10-03 11:53:59 -0300

    Django

---
+++ olaMundo/api/urls.py
@@ -0,0 +1,12 @@
+
+from django.urls import path
+from api.views import index, apiOla
+
+urlpatterns = [
+     path('api/ola/', apiOla),
+     path('', index),
+     
+
+
+
+]
---
+++ olaMundo/api/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+# JSON API
+@api_view(['GET'])
+def apiOla(request):
+    return Response("Olá Mundo!")
